d97a5f3d3daaeb99e97f0fe08e3f1897c2b48671
Koshal Kishor <133968845+kaushal9643@users.noreply.github.com>
2026-04-30 11:46:06 +0530

    Add: remove-node-from-end-of-linked-list - submission-1

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-1.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head == null) return head;
+        ListNode temp = head;
+        int size = 0;
+        while(temp != null){
+            size++;
+            temp = temp.next;
+        } 
+
+        int rem = size-n;
+
+        if(rem == 0) return head.next;
+
+        temp = head;
+        ListNode prev = null;
+        while(rem != 0){
+            prev = temp;
+            temp = temp.next;
+            rem--;
+        }
+        prev.next = temp.next;
+
+        return head;
+    }
+}
